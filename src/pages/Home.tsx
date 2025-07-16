a471f33bcec07f194f35d3adb3409ddb3928ea81
Ariel Jr Mimura <avmimurajr@gmail.com>
2025-07-16 16:56:39 +0800

    CHCK: Removed unnecessary code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,9 @@ import { CareerSection } from "../components/CareerSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/FooterSection";
-// import { HoneycombBackground } from "../components/HoneycombBackground";
 
 export const Home = () => (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
-        {/* {<HoneycombBackground />} */}
 
         {<ThemeToggle />}
 
